76ca1616ec8c4d94ef2f0853a1de59bc6233076d
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-10-16 21:55:10 +0800

    remove target directory

--- common-lib/src/main/java/com/simple/common/config/SimpleRestConfig.java
+++ common-lib/src/main/java/com/simple/common/config/SimpleRestConfig.java
@@ -1,6 +1,7 @@
 package com.simple.common.config;
 
 import com.simple.common.aop.SentryClientAspect;
+import com.simple.core.redis.RedisClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import com.simple.common.error.GlobalExceptionTranslator;
--- common-lib/src/main/java/com/simple/core/redis/RedisClient.java
+++ common-lib/src/main/java/com/simple/core/redis/RedisClient.java
@@ -5,30 +5,30 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-
 @Configuration
 public class RedisClient {
        public static ValueOperations<String, Object> operatorInstance;
        public static RedisTemplate<String, Object> templateInstance;
+//
+//        @Bean
+//        JedisConnectionFactory connectionFactory() {
+//            return new JedisConnectionFactory();
+//        }
+
+//        @Bean
+//        ValueOperations<String, String> strOperations(RedisTemplate<String, String> redisTemplate) {
+//            return redisTemplate.opsForValue();
+//        }
 
         @Bean
-        JedisConnectionFactory connectionFactory() {
-            return new JedisConnectionFactory();
-        }
-
-        @Bean
-        ValueOperations<String, String> strOperations(RedisTemplate<String, String> redisTemplate) {
-            return redisTemplate.opsForValue();
-        }
-
-        @Bean
-        RedisTemplate<String, Integer> intRedisTemplate(JedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Integer> intRedisTemplate(RedisConnectionFactory connectionFactory) {
             RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<String, Integer>();
             redisTemplate.setConnectionFactory(connectionFactory);
             return redisTemplate;
@@ -48,7 +48,7 @@ public class RedisClient {
         }
 
         @Bean
-        RedisTemplate<String, Object> objRedisTemplate(JedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> objRedisTemplate(RedisConnectionFactory connectionFactory) {
             RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
             redisTemplate.setConnectionFactory(connectionFactory);
 
