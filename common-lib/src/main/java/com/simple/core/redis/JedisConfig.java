d338b8ce886b846bb5be0eb73791ad8ef059be36
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-10-19 23:07:42 +0800

    remove target directory

--- common-lib/src/main/java/com/simple/core/redis/JedisConfig.java
+++ common-lib/src/main/java/com/simple/core/redis/JedisConfig.java
@@ -20,32 +20,32 @@ public class JedisConfig {
     /**检测获取资源是有有效*/
     public static Boolean redis_testOnBorrow;
 
-    @Value("${redis.hostName}")
+    @Value("${redis.hostName:localhost}")
     public void setRedis_hostName(String redis_hostName) {
         JedisConfig.redis_hostName = redis_hostName;
     }
 
-    @Value("${redis.redisport}")
+    @Value("${redis.redisport:6379}")
     public void setRedis_hostPort(Integer redis_hostPort) {
         JedisConfig.redis_hostPort = redis_hostPort;
     }
 
-    @Value("${redis.maxActive}")
+    @Value("${redis.maxActive:500}")
     public void setRedis_maxActive(Integer redis_maxActive) {
         JedisConfig.redis_maxActive = redis_maxActive;
     }
 
-    @Value("${redis.maxIdle}")
+    @Value("${redis.maxIdle:100}")
     public void setRedis_maxIdle(Integer redis_maxIdle) {
         JedisConfig.redis_maxIdle = redis_maxIdle;
     }
 
-    @Value("${redis.maxWait}")
+    @Value("${redis.maxWait:1000}")
     public void setRedis_maxWait(Integer redis_maxWait) {
         JedisConfig.redis_maxWait = redis_maxWait;
     }
 
-    @Value("${redis.testOnBorrow}")
+    @Value("${redis.testOnBorrow:true}")
     public void setRedis_testOnBorrow(Boolean redis_testOnBorrow) {
         JedisConfig.redis_testOnBorrow = redis_testOnBorrow;
     }
